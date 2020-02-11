2ff25d5d367f9232c30c6b2cf4fc02de3eb1fd66
agarwalSuyash <59922583+agarwalSuyash@users.noreply.github.com>
2020-02-11 21:32:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+// navbar diapperar
 var newLocation;
 var prevScrollpos = window.pageYOffset;
+mybutton = document.getElementById("myBtn");
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "0";             //navbar styling
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+      mybutton.style.display = "block";                           //scroll top button
+    }
+    else
+    {
+      mybutton.style.display = "none";  
+    }
   } else {
-    document.getElementById("navbar").style.top = "-150px";
-  }
+    document.getElementById("navbar").style.top = "-150px";      //navbar styling
+    mybutton.style.display = "none";                            //scroll top button
+    }
   prevScrollpos = currentScrollPos;
 }
 
@@ -19,4 +30,11 @@ $('body').fadeOut('slow', newpage);
 });
 function newpage() {
 window.location = newLocation;
+}
+
+// scroll to top button
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
